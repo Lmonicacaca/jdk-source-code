dce0d7a3e914542c9c150882116ed9aae7348c43
lilin <396435308@qq.com>
2019-09-27 13:54:08 +0800

    添加redis和dubbo配置

--- src/main/java/com/jdk_source_code/demo/DemoApplication.java
+++ src/main/java/com/jdk_source_code/demo/DemoApplication.java
@@ -1,6 +1,6 @@
 package com.jdk_source_code.demo;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
