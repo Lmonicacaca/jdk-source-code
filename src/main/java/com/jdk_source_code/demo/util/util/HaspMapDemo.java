c08cc4175ec1ba2874593d71280747aa998293c6
lilin <396435308@qq.com>
2019-10-30 15:29:45 +0800

    HashMap

---
+++ src/main/java/com/jdk_source_code/demo/util/util/HaspMapDemo.java
@@ -0,0 +1,15 @@
+package com.jdk_source_code.demo.util.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lilin
+ * @date 2019-10-23
+ */
+public class HaspMapDemo {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("123","123");
+    }
+}
