5f3465547229a6929f6310ad6bada1c8a407dfea
lilin <396435308@qq.com>
2019-11-06 11:24:24 +0800

    HashMap

--- src/main/java/com/jdk_source_code/demo/util/util/HaspMapDemo.java
+++ src/main/java/com/jdk_source_code/demo/util/util/HaspMapDemo.java
@@ -1,5 +1,6 @@
 package com.jdk_source_code.demo.util.util;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,5 +12,16 @@ public class HaspMapDemo {
     public static void main(String[] args) {
         Map<String,String> map = new HashMap<>();
         map.put("123","123");
+        map.put("333","333");
+        map.put("444","333");
+        map.put("555","333");
+        map.put("666","333");
+        map.put("777","777");
+        map.put("888","888");
+        map.put("999","999");
+        map.put("000","000");
+        map.put("232","232");
+        map.put("343","343");
+        map.put("454","454");
     }
 }
